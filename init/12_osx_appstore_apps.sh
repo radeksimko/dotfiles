2a6d1ed70a7cde99914d248fb51eafb910369aea
Radek Simko <radek.simko@gmail.com>
2014-04-27 12:51:59 +0100

    Personal stuff added

---
+++ init/12_osx_appstore_apps.sh
@@ -0,0 +1,22 @@
+# OSX-only stuff. Abort if not OSX.
+[[ "$OSTYPE" =~ ^darwin ]] || return 1
+
+e_header "Installing apps from AppStore"
+
+# https://itunes.apple.com/gb/app/twitter/id409789998?mt=12
+
+# https://itunes.apple.com/gb/app/the-unarchiver/id425424353?mt=12
+
+# https://itunes.apple.com/gb/app/battery-time-remaining/id551420833?mt=12
+
+# # Growl
+# https://itunes.apple.com/gb/app/growl/id467939042?mt=12
+
+# # SourceTree
+# https://itunes.apple.com/gb/app/sourcetree-git-hg/id411678673?mt=12
+
+# # Window Magnet
+# https://itunes.apple.com/gb/app/window-magnet/id441258766?mt=12
+
+# # Caffeine
+# https://itunes.apple.com/gb/app/caffeine/id411246225?mt=12
---
+++ init/50_development.sh
@@ -0,0 +1,5 @@
+
+e_header "Installing autocompletion"
+
+e_arrow "Installing Git completion"
+curl https://raw.github.com/git/git/master/contrib/completion/git-completion.bash > $HOME/.git-completion.bash
