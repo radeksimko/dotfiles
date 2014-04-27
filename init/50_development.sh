582ac11926db32e9515f6033525836109dc09ca4
Radek Simko <radek.simko@gmail.com>
2014-04-27 16:34:18 +0100

    Personal stuff removed
    This reverts commit 2a6d1ed70a7cde99914d248fb51eafb910369aea.

--- init/50_development.sh
+++
@@ -1,5 +0,0 @@
-
-e_header "Installing autocompletion"
-
-e_arrow "Installing Git completion"
-curl https://raw.github.com/git/git/master/contrib/completion/git-completion.bash > $HOME/.git-completion.bash
