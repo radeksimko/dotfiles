81560352d1518f9f0214b20ff1e59832c06b547e
Radek Simko <radek.simko@gmail.com>
2014-05-04 21:39:45 +0100

    AppStore apps added

--- init/12_osx_appstore_apps.sh
+++ init/12_osx_appstore_apps.sh
@@ -1,22 +1,13 @@
-# OSX-only stuff. Abort if not OSX.
-[[ "$OSTYPE" =~ ^darwin ]] || return 1
+# AppStore apps
 
-e_header "Installing apps from AppStore"
+apps=`cat <<HELP
+  the-unarchiver/id425424353 "The Unarchiver.app"
+  twitter/id409789998 Twitter.app
+  battery-time-remaining/id551420833 "Battery Time Remaining.app"
+  growl/id467939042 Growl.app
+  sourcetree-git-hg/id411678673 SourceTree.app
+  window-magnet/id441258766 "Window Magnet.app"
+  caffeine/id411246225 Caffeine.app
+HELP`
 
-# https://itunes.apple.com/gb/app/twitter/id409789998?mt=12
-
-# https://itunes.apple.com/gb/app/the-unarchiver/id425424353?mt=12
-
-# https://itunes.apple.com/gb/app/battery-time-remaining/id551420833?mt=12
-
-# # Growl
-# https://itunes.apple.com/gb/app/growl/id467939042?mt=12
-
-# # SourceTree
-# https://itunes.apple.com/gb/app/sourcetree-git-hg/id411678673?mt=12
-
-# # Window Magnet
-# https://itunes.apple.com/gb/app/window-magnet/id441258766?mt=12
-
-# # Caffeine
-# https://itunes.apple.com/gb/app/caffeine/id411246225?mt=12
+INSTALL_APPSTORE_APPS "$apps"
