a6946a74cabfb3fcee60f10fd5fa8d24222079b5
Radek Simko <radek.simko@gmail.com>
2014-05-04 21:41:10 +0100

    Heredoc name changed

--- init/12_osx_appstore_apps.sh
+++ init/12_osx_appstore_apps.sh
@@ -1,6 +1,6 @@
 # AppStore apps
 
-apps=`cat <<HELP
+apps=`cat <<LIST
   the-unarchiver/id425424353 "The Unarchiver.app"
   twitter/id409789998 Twitter.app
   battery-time-remaining/id551420833 "Battery Time Remaining.app"
@@ -8,6 +8,6 @@ apps=`cat <<HELP
   sourcetree-git-hg/id411678673 SourceTree.app
   window-magnet/id441258766 "Window Magnet.app"
   caffeine/id411246225 Caffeine.app
-HELP`
+LIST`
 
 INSTALL_APPSTORE_APPS "$apps"
