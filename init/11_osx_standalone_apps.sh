555aacbeaee3bee160347a612647501740fa8869
Radek Simko <radek.simko@gmail.com>
2014-05-11 20:49:08 +0100

    Refactored to use install helpers

--- init/11_osx_standalone_apps.sh
+++ init/11_osx_standalone_apps.sh
@@ -1,90 +1,50 @@
-# OSX-only stuff. Abort if not OSX.
-[[ "$OSTYPE" =~ ^darwin ]] || return 1
-
-e_header "Installing standalone apps"
-
-sudo -v
-
-function INSTALL_DMG() {
-  local install_type=$1
-  local url=$2
-  local id=`md5 -s $url`
-  local dmgLocation="$HOME/Downloads/dotfiles-$id.dmg"
-
-  e_arrow "Downloading DMG from $url"
-  curl $url > $dmgLocation
-  e_arrow "Mounting DMG"
-  hdiutil attach -mountpoint $id $dmgLocation
-
-  if [[ $install_type == '--copy_app' ]]; then
-    e_arrow "Installing by copying app"
-    cp -R /Volumes/$id/*.app /Applications/
-  elif [[ $install_type == '--run_app' ]]; then
-    e_arrow "Installing by opening app"
-    open /Volumes/$id/*.app
-  elif [[ $install_type == '--install_pkg' ]]; then
-    e_arrow "Installing via pkg"
-    sudo -S installer -verbose -pkg *.pkg -target /
-  else
-    e_error "Provide known install_type"
-  fi;
-
-  e_arrow "Unmounting DMG"
-  hdiutil detach "/Volumes/$id"
-  e_arrow "Deleting DMG"
-  rm -f $dmgLocation
-}
-
-function UNZIP_APP() {
-  local url, id
-  url=$1
-  id=`md5 -s $url`
-
-  e_arrow "Downloading ZIP from $url"
-  curl $url > ~/Downloads/dotfiles-$id.zip
-  e_arrow "Unzipping content"
-  unzip $1 -d /Applications
-  e_arrow "Removing previously downloaded ZIP"
-  rm -f ~/Downloads/dotfiles-$id.zip
-}
-
 # Google Chrome
-INSTALL_DMG --copy_app https://dl.google.com/chrome/mac/stable/GGRO/googlechrome.dmg
+INSTALL_DMG --copy_app 'https://dl.google.com/chrome/mac/stable/GGRO/googlechrome.dmg' 'Google Chrome.app'
 
 # Firefox
-INSTALL_DMG --copy_app 'https://download.mozilla.org/?product=firefox-27.0.1-SSL&os=osx&lang=en-US'
-
-# Opera
-INSTALL_DMG --just_mount 'http://www.opera.com/download/get/?id=36437&location=360&nothanks=yes&sub=marine'
-
-# Adobe Reader
-#https://get.adobe.com/uk/reader/download/?installer=Reader_11.0.06_English_UK_for_Mac_Intel&os=OSX&browser_type=KHTML&browser_dist=Chrome&d=Adobe_Photoshop_Lightroom_for_Macintosh&dualoffer=false
-
-# Flash Player
+INSTALL_DMG --copy_app 'https://download.mozilla.org/?product=firefox-27.0.1-SSL&os=osx&lang=en-US' 'Firefox.app'
 
 # Android File Transfer
-INSTALL_DMG --copy_app https://dl.google.com/dl/androidjumper/mtp/current/androidfiletransfer.dmg
+INSTALL_DMG --copy_app 'https://dl.google.com/dl/androidjumper/mtp/current/androidfiletransfer.dmg' 'Android File Transfer.app'
 
 # Adium
-INSTALL_DMG --copy_app http://kent.dl.sourceforge.net/project/adium/Adium_1.5.9.dmg
+INSTALL_DMG --copy_app "http://downloads.sourceforge.net/project/adium/Adium_1.5.9.dmg?r=&ts=`date +"%s"`&use_mirror=garr" 'Adium.app'
 
 # Google Drive
-INSTALL_DMG --copy_app https://dl.google.com/drive/installgoogledrive.dmg
-
-# Last.fm
-UNZIP_APP http://www.last.fm/download/mac
+INSTALL_DMG --copy_app 'https://dl.google.com/drive/installgoogledrive.dmg' 'Google Drive.app'
 
 # VLC
-INSTALL_DMG --copy_app http://videolan.mirrors.uk2.net/vlc/2.1.4/macosx/vlc-2.1.4.dmg
+INSTALL_DMG --copy_app 'http://videolan.mirrors.uk2.net/vlc/2.1.4/macosx/vlc-2.1.4.dmg' 'VLC.app'
 
-# iTerm
-UNZIP_APP http://www.iterm2.com/nightly/latest
+# SourceTree
+INSTALL_DMG --copy_app 'http://downloads.atlassian.com/software/sourcetree/SourceTree_1.9.1.dmg' 'SourceTree.app'
+
+# Sublime Text
+INSTALL_DMG --copy_app 'http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%20Build%203059.dmg' 'Sublime Text.app'
+
+# Disk Inventory X
+INSTALL_DMG --copy_app 'http://www.alice-dsl.net/tjark.derlien/DIX1.0Universal.dmg' 'Disk Inventory X.app'
+
+# Skype
+INSTALL_DMG --copy_app 'http://download.skype.com/macosx/Skype_6.15.0.330.dmg' 'Skype.app'
+
+# Viscosity
+INSTALL_DMG --copy_app 'http://www.sparklabs.com/downloads/Viscosity.dmg' 'Viscosity.app'
 
 # Virtualbox
-INSTALL_DMG --install_pkg http://download.virtualbox.org/virtualbox/4.3.8/VirtualBox-4.3.8-92456-OSX.dmg
+INSTALL_DMG --install_pkg 'http://download.virtualbox.org/virtualbox/4.3.8/VirtualBox-4.3.8-92456-OSX.dmg' 'VirtualBox.app'
 
 # Vagrant
-INSTALL_DMG --install_pkg https://dl.bintray.com/mitchellh/vagrant/Vagrant-1.4.0.dmg
+INSTALL_DMG --install_pkg 'https://dl.bintray.com/mitchellh/vagrant/Vagrant-1.4.0.dmg' 'Vagrant'
+
+# Last.fm
+UNZIP_APP 'http://www.last.fm/download/mac' 'Last.fm.app'
+
+# iTerm
+UNZIP_APP 'http://www.iterm2.com/nightly/latest' 'iTerm.app'
+
+# Adobe Reader
+INSTALL_DMG --run_app 'http://aihdownload.adobe.com/bin/live/AdobeReaderInstaller_11_uk_ltrosxd_aaa_aih.dmg' 'Adobe Reader.app'
 
 # Dropbox
-INSTALL_DMG --run_app 'https://www.dropbox.com/download?src=index&plat=mac'
+INSTALL_DMG --run_app 'https://www.dropbox.com/download?src=index&plat=mac' 'Dropbox.app'
