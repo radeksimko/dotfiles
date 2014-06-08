d8e2615b4f145d50acc27ee8b6289715cc7a7e2b
Radek Simko <radek.simko@gmail.com>
2014-06-08 13:02:23 +0100

    NPM + PIP packages + gems added

--- init/50_osx_development.sh
+++ init/50_osx_development.sh
@@ -1,5 +1,14 @@
+e_arrow "Checking su permissions"
+sudo -v
 
-e_header "Installing autocompletion"
+e_arrow "Installing PIP packages"
+sudo pip install -q -r ~/.dotfiles/conf/pip/requirements.txt
 
-e_arrow "Installing Git completion"
-curl https://raw.github.com/git/git/master/contrib/completion/git-completion.bash > $HOME/.git-completion.bash
+e_arrow "Installing NPM packages"
+cd ~/.dotfiles/conf/npm/
+sudo npm install -g
+
+e_arrow "Installing Ruby gems"
+sudo gem install -q bundler
+cd ~/.dotfiles/conf/gem/
+/usr/local/opt/ruby/bin/bundle install --system
