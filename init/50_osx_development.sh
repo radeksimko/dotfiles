2f146c0400098208653c1932d331d0fbd38c8941
Radek Simko <radek.simko@gmail.com>
2014-06-08 18:08:54 +0100

    Quiet switchers removed

--- init/50_osx_development.sh
+++ init/50_osx_development.sh
@@ -2,13 +2,13 @@ e_arrow "Checking su permissions"
 sudo -v
 
 e_arrow "Installing PIP packages"
-sudo pip install -q -r ~/.dotfiles/conf/pip/requirements.txt
+sudo pip install -r ~/.dotfiles/conf/pip/requirements.txt
 
 e_arrow "Installing NPM packages"
 cd ~/.dotfiles/conf/npm/
 sudo npm install -g
 
 e_arrow "Installing Ruby gems"
-sudo gem install -q bundler
+sudo gem install bundler
 cd ~/.dotfiles/conf/gem/
 /usr/local/bin/bundle install --system
