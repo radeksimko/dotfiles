923b406b297dee9ea94b1c3bafe80e9ae88cd035
Radek Simko <radek.simko@gmail.com>
2014-06-08 12:06:19 +0100

    Some AppStore apps are available through Homebrew Cask (= much easier, less work)

--- init/12_osx_appstore_apps.sh
+++ init/12_osx_appstore_apps.sh
@@ -1,13 +1,9 @@
 # AppStore apps
 
 apps=`cat <<LIST
-  the-unarchiver/id425424353 "The Unarchiver.app"
   twitter/id409789998 Twitter.app
-  battery-time-remaining/id551420833 "Battery Time Remaining.app"
   growl/id467939042 Growl.app
-  sourcetree-git-hg/id411678673 SourceTree.app
   window-magnet/id441258766 "Window Magnet.app"
-  caffeine/id411246225 Caffeine.app
   xcode/id497799835 Xcode.app
 LIST`
 
